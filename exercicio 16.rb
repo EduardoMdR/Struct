a94f7104eaa68f74cd100db6935090dda140d5d4
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-05-08 22:20:03 -0300

    Update exercicio 16.rb

--- exercicio 16.rb
+++ exercicio 16.rb
@@ -10,7 +10,7 @@ C-G             #e C com G
 
 # o Exercicio gera uma primeira fita de DNA, após isso completa a fita com seu par
 
-def combinacao(letra)               # a função secundaria imprime os resultados desejados     
+def combinacao(letra)               # a função imprime os resultados desejados     
     if letra == "A"
         puts "[A, T]"
     elsif letra == "T"
@@ -38,12 +38,12 @@ end
 print "]\n"
 
 puts "DNA:"
-for i in 0..9                       # na função principal eu chamaria a função secundaria
+for i in 0..9
     combinacao(fita[salvar[i]])
 end
 
 =begin      Uma outra maneira de resolver
-Como não vou salvar os dados eu posso apenas imprimir os dados de acordo com a fita principal gerada
+Como não vou salvar os dados eu posso apenas imprimir os pares de acordo com a fita principal gerada
 
 mas caso quisesse poderia fazer uma nova lista (array) com os pares de dados
 Ex:
@@ -54,6 +54,3 @@ lista = [
 quando eu utilizo fita[salvar[i]], quer dizer que vou pegar um numero aleatorio (em 'salvar') de 0 a 3 que me devolve a letra correspondente ao numero
 0 = A, 1 = T, 2 = C, 3 = G
 =end
-
-
-
