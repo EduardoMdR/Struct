d6016be4a5040dc119520ec30a26aedfe06a817e
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-05-08 23:35:39 -0300

    Comentario

--- desafio 1.rb
+++ desafio 1.rb
@@ -1,3 +1,4 @@
+# Não é o RU
 restaurante = {}
 restaurante['entrada'] = ["Salada","Sopa","Legumes"]
 restaurante['principal']=["Canja de Galinha","Ensopado de bode","Estrogonof","batata assada","camarão"]
@@ -9,3 +10,4 @@ restaurante['sobremesa']=["Torda de Limão","Bolo de Cenoura","Sorvete de Creme"
 print "[#{restaurante['entrada'][rand(3)]}] "
 print "[#{restaurante['principal'][rand(5)]}] "
 print "[#{restaurante['sobremesa'][rand(4)]}]"
+# vou selecionar aleatoriamente os pratos principais
