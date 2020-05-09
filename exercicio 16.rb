4aa3cb0e5203e6ef1671e2e38df7e2c37f5f0638
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-05-08 22:19:18 -0300

    Update exercicio 16.rb

--- exercicio 16.rb
+++ exercicio 16.rb
@@ -1,4 +1,4 @@
-=begin  Explicação basica
+=begin      Explicação basica
 
 adenina(A)      #nucleotídeos
 timina(T)   
@@ -9,33 +9,6 @@ C-G             #e C com G
 =end
 
 # o Exercicio gera uma primeira fita de DNA, após isso completa a fita com seu par
-
-
-
-fita ={}                            #cria um hash para a primeira fita
-salvar = {}                         #cria um hash para salvar a aleatoridade da fita
-salvar['num'] = [rand(4),rand(4),rand(4),rand(4),rand(4),rand(4),rand(4),rand(4),rand(4),rand(4),]
-# puts salvar
-
-fita['nucleotideos'] = ["A","T","C","G"]
-puts "Primeira fita:"
-print "["
-for i in 0..9
-    print "#{fita['nucleotideos'][salvar['num'][i]]} "
-end
-print "]\n"
-puts "DNA:"
-
-
-
-
-=begin      Uma outra maneira de resolver
-Como não vou salvar os dados eu poderia apenas imprimir na tela o resultado esperado
-
-for i in 0..9                       # na função principal eu chamaria a função secundaria
-    combinacao(fita['nucleotideos'][salvar['num'][i]])
-end
-
 
 def combinacao(letra)               # a função secundaria imprime os resultados desejados     
     if letra == "A"
@@ -49,6 +22,37 @@ def combinacao(letra)               # a função secundaria imprime os resultado
     end
 end
 
+
+#cria um hash para salvar a aleatoridade da fita
+salvar = [rand(4),rand(4),rand(4),rand(4),rand(4),rand(4),rand(4),rand(4),rand(4),rand(4),]
+# puts salvar
+
+# criar o array de nucleotideos
+fita = ["A","T","C","G"]
+
+puts "Primeira fita:"
+print "["
+for i in 0..9
+    print "#{fita[salvar[i]]} "
+end
+print "]\n"
+
+puts "DNA:"
+for i in 0..9                       # na função principal eu chamaria a função secundaria
+    combinacao(fita[salvar[i]])
+end
+
+=begin      Uma outra maneira de resolver
+Como não vou salvar os dados eu posso apenas imprimir os dados de acordo com a fita principal gerada
+
+mas caso quisesse poderia fazer uma nova lista (array) com os pares de dados
+Ex:
+lista = [
+['A','T'],['T','A']['C','G']['G','C']
+]
+
+quando eu utilizo fita[salvar[i]], quer dizer que vou pegar um numero aleatorio (em 'salvar') de 0 a 3 que me devolve a letra correspondente ao numero
+0 = A, 1 = T, 2 = C, 3 = G
 =end
 
 
