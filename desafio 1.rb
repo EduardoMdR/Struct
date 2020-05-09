df019cd363654d7e886ce7c40c2ca45ddd028c4f
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-05-08 21:36:33 -0300

    Update desafio 1.rb

--- desafio 1.rb
+++ desafio 1.rb
@@ -4,25 +4,8 @@ restaurante['principal']=["Canja de Galinha","Ensopado de bode","Estrogonof","ba
 restaurante['sobremesa']=["Torda de Limão","Bolo de Cenoura","Sorvete de Creme","Pudim"]
 #criei um hash e atribui as chaves e os valores deles
 
- puts restaurante
+# puts restaurante
 
-
-restaurante.each do |key,value|
-    puts key
-    puts value
-end
 print "[#{restaurante['entrada'][rand(3)]}] "
 print "[#{restaurante['principal'][rand(5)]}] "
 print "[#{restaurante['sobremesa'][rand(4)]}]"
-
-
-
-
-
-
-
-
-
-#3
-#5
-#4
