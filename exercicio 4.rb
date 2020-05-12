b1c26c22d33eb5102c24f4ada0878f6df06a16c2
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-05-12 15:04:18 -0300

    Delete exercicio 4.rb

--- exercicio 4.rb
+++
@@ -1,7 +0,0 @@
-# puts "Digite seu nome, sua idade, seu peso e sua altura"
-nome = "Eduardo"
-idade = 20          #por não utilizar aspas, o programa entendo que não é uma string
-peso =  56.5        #utilizando o ponto transforma em float
-altura = 1.76       
-
-puts "Meu nome é #{nome}, tenho #{idade} anos, peso #{peso} Kg e minha altura é #{altura} m"
