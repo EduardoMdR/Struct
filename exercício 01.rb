f3b5500df9cc993a29d45737cb54c35d20246379
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-05-12 15:05:08 -0300

    Delete exercício 01.rb

--- exercício 01.rb
+++
@@ -1,6 +0,0 @@
-#somar 5 ao numero recebido
-numero = gets.to_i
-
-puts "Recebi: #{numero}"
-puts "Resultado: #{numero + 5}"
-#poderia criar uma variavel para receber o valor ou fazer direto na saida
