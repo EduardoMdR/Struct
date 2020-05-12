23aa3af30e4f99cf4aa697990243beddca0e9be1
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-05-12 15:03:58 -0300

    Delete exercicio 2.rb

--- exercicio 2.rb
+++
@@ -1,9 +0,0 @@
-# puts "digite a frase desejada"
-palavra = gets
-palavra = palavra.ascii_only?                   # .ascii_only? verifica se a variavel em questão contem apenas caracteres ascii
-
-if palavra == true
-    puts "Contém somente caracteres ASCII"
-else
-    puts "Contém caracteres não ASCII"
-end
