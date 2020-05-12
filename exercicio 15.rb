fe0efa8e9baa22d4eed3feda2b810d068ef3e88b
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-05-12 15:03:29 -0300

    Delete exercicio 15.rb

--- exercicio 15.rb
+++
@@ -1,8 +0,0 @@
-informacoes ={}                                   #hash vazio ou Hash.new
-
-informacoes['nome'] = gets.to_s.strip             # .strip serve para não pegar o \n
-informacoes['idade'] = gets.to_i
-informacoes['matricula'] = gets.to_i
-informacoes['email'] = gets.to_s.strip
-
-puts informacoes
